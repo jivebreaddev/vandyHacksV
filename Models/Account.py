cdb874e1ba22edf142103d555aef3314a346e701
la96bikal <blamichhane@troy.edu>
2018-11-03 02:20:27 -0500

    Models and Template

---
+++ Models/Account.py
@@ -0,0 +1,12 @@
+class Account:
+    def __init__(self, Fname, Lname, email, phone, username, password):
+        self.Fname = Fname
+        self.Lname = Lname
+        self.email = email
+        self.phone = phone
+        self.username = username
+        self.password = password
+
+    def toParams(self):
+        params = [self.Fname, self.Lname, self.email, self.phone, self.username, self.password]
+        return params
