ec7ea8a2dd0f96fb86bf01c0dbba3d9aeaaa7b20
Jivebread <jbnub12@gmail.com>
2018-11-03 23:30:00 -0500

    algo

--- coAPI/evenbritedata.py
+++ coAPI/evenbritedata.py
@@ -32,28 +32,28 @@ class dataloader:
 
             self.event_ticket_list.append(event_ticket(event['id'],organization_id))
 
-            self.load()
-            self.algo_implementation_1 = algo(10, 5, self.event_ticket_list[0], self.eventbrite_api)
+            self.load(event)
+            self.load_algo(5000,500)
 
             self.algo_implementation_1.incremental_algo()
         print(len(events))
     # return events_list
 
 
-    def load(self):
-        for event_ticket in self.event_ticket_list:
-            # print(event_ticket.event_id)
-            event = self.eventbrite_api.get_event(event_ticket.event_id)
-            print(event)
-            event_ticket.event_details(event["name"], event["url"])
-            event_ticket.event_time_load(datetime.now(),datetime.strptime(event["end"]["utc"],"%Y-%m-%dT%H:%M:%SZ"))
+    def load(self, event_ticket):
+
+        # print(event_ticket.event_id)
+        event = self.eventbrite_api.get_event(event_ticket.event_id)
+
+        event_ticket.event_details(event["name"], event["url"])
+        event_ticket.event_time_load(datetime.now(),datetime.strptime(event["end"]["utc"],"%Y-%m-%dT%H:%M:%SZ"))
     def load_list(self):
         list_mark = self.event_ticket_list[:2]
         return list_mark
     # def delete_tickets(self, event_id, ticket_class_id):
     #     self.eventbrite_api.delete_ticket_class(event_id,ticket_class_id)
-    def load_algo(self):
-        self.algo_implementation_1 = algo(10, 5, self.event_ticket_list[0], self.eventbrite_api)
+    def load_algo(self,price, change):
+        self.algo_implementation_1 = algo(price, change, self.event_ticket_list[0], self.eventbrite_api)
 
         self.algo_implementation_1.incremental_algo()
 
