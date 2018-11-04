5cbf3dbe87905a673f3e47914104d9c44865acc6
Saurabh Bhandari <bhandarisaurav07@gmail.com>
2018-11-03 19:08:41 -0500

    some changes

--- coAPI/fetchData.py
+++ coAPI/fetchData.py
@@ -1,18 +1,26 @@
 import requests
-customerURL = "http://api.reimaginebanking.com/customers?key=f6b2994f930f3175da90249976afd314"
-merchantURL = 'http://api.reimaginebanking.com/merchants?key=f6b2994f930f3175da90249976afd314'
-r1 = requests.get(url=customerURL, params=None)
-r2 = requests.get(url=merchantURL, params=None)
-data1 = r1.json()
-data2 = r2.json()
-# for elem in data1:
-#     email = elem['last_name']+'@' + elem['first_name'] + '.com'
-#     print(email)
-#     print(elem['last_name'], elem['first_name'])
-for elem in data2:
-    name = elem['name'].replace(" ", "")
-    email = name+'@email.com'
-    print(email, sep='')
-#     print(elem['name'])
-# print(len(data1))
-# print(len(data2))
+
+
+def merchant_data():
+    merchant_url = 'http://api.reimaginebanking.com/merchants?key=f6b2994f930f3175da90249976afd314'
+    merchant = {
+        'name': "email",
+    }
+    r2 = requests.get(url=merchant_url, params=None)
+    data2 = r2.json()
+    for elem in data2:
+        name = elem['name']
+        email_id = elem['_id']
+        mer_email = email_id + '@gmail.com'
+        merchant.update({
+            name: mer_email
+        })
+    return merchant
+
+
+def main():
+    merchant_data()
+
+
+if __name__ == '__main__':
+    main()
