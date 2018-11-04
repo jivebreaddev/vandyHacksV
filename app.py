dbc7c5a3c038399173d6ee8136367cdd24c6e29f
Saurabh Bhandari <bhandarisaurav07@gmail.com>
2018-11-04 08:35:14 -0600

    Merge branch 'master' of https://github.com/SBtechie/vandyHacksV

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
 from flask import Flask, render_template,request, jsonify
 import DAL.DBOps as db
 import Models.Models as M
+import coAPI.evenbritedata as SP
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-
     return render_template('home.html')
 
 @app.route('/signin', methods = ['POST', 'GET'])
 def signin():
     Form = request.form
-    events = ['Event 1', 'Event 2', 'Event 3', 'Event 4', 'Event 5']
+    acc = db.retrieveAccount('Spark')
+    events = SP.dataloader(acc.oAuth).load_list()
+    events = list(set(events))
     if request.method == 'POST':
         auth = db.Authenticate(Form['username'], Form['password'])
         if auth:
+            print('I am authenticated')
             return render_template('profile.html', acc = db.retrieveAccount(Form['username']), event = events)
         else:
             return render_template('signin.html', Message="Invalid Username or Password. Try Again")
 
     return render_template('signin.html', form=Form)
 
-@app.route('/register', methods=['POST','GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     form1 = request.form
 
 
     if request.method == 'POST':
-        acc = M.Models(form1['Fname'], form1['Lname'], form1['email'], form1['phone'], form1['username'],
+        acc = M.Account(form1['Fname'], form1['Lname'], form1['email'], form1['phone'], form1['username'],
                        form1['password'], form1['oAuth'])
         db.insertAccount(acc)
         return render_template('signin.html', form=form1)
@@ -35,21 +38,27 @@ def register():
 
     return render_template('register.html', form=form1)
 
-# @app.route('/profile', methods = ['POST'])
-# def profile():
-#     print('I GOT HERE')
-#     form2 = request.form
-#     custs = [M.Customer('Bikal', 'sdf@gmail.com'), M.Customer('Saurabh', 'bhandari@gmail.com')]
-#     if request.method == 'POST':
-#
-#         return render_template('Customers.html', custos = custs)
-#     return render_template('profile.html', form = form2)
+
+
 
 @app.route('/customers')
 def customers():
+    # if request.method == 'POST':
+    #     form2 = request.form
+    #     jsonO = request.get_json()
+    #     print(jsonO)
+    #     return "trying things out"
+
     userN = request.args.get('my_var', None)
     custs = [M.Customer('Bikal', 'sdf@gmail.com'), M.Customer('Saurabh', 'bhandari@gmail.com')]
     return render_template('Customers.html', cust = custs)
+
+
+@app.route('/pricing')
+def pricing():
+    obj = request.get_json()
+    print(obj)
+    return "Hellow"
 
 if __name__ == '__main__':
     app.run(debug = True)
---
+++ coAPI/evenbritedata.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+import eventbrite
+
+from Models.Models import event_ticket, algo
+
+
+class dataloader:
+    oAuth = None
+    event_ticket_list = list()
+    eventbrite_api = None
+    algo_implementation_1 = None
+    algo_implementation_2 = None
+
+    def __init__(self,oAuth):
+        self.eventbrite_api = eventbrite.Eventbrite(oAuth)
+
+
+
+        user_id = self.eventbrite_api.get_user()
+        organizatino_id = self.eventbrite_api.get_organization(user_id=user_id.id)
+
+        print(organizatino_id['organizations'])
+        id_organization = [organization['id'] for organization in organizatino_id['organizations']]
+
+        for organization_id in id_organization:
+            organization_check = self.eventbrite_api.get_organization_events(organization_id=organization_id)
+
+            events = organization_check["events"]
+        organization_id = id_organization[0]
+        for event in events:
+
+            self.event_ticket_list.append(event_ticket(event['id'],organization_id))
+
+            self.load(event)
+            self.load_algo(5000,500)
+
+            self.algo_implementation_1.incremental_algo()
+        print(len(events))
+    # return events_list
+
+
+    def load(self, event_ticket):
+
+        # print(event_ticket.event_id)
+        event = self.eventbrite_api.get_event(event_ticket.event_id)
+
+        event_ticket.event_details(event["name"], event["url"])
+        event_ticket.event_time_load(datetime.now(),datetime.strptime(event["end"]["utc"],"%Y-%m-%dT%H:%M:%SZ"))
+    def load_list(self):
+        list_mark = self.event_ticket_list[:2]
+        return list_mark
+    # def delete_tickets(self, event_id, ticket_class_id):
+    #     self.eventbrite_api.delete_ticket_class(event_id,ticket_class_id)
+    def load_algo(self,price, change):
+        self.algo_implementation_1 = algo(price, change, self.event_ticket_list[0], self.eventbrite_api)
+
+        self.algo_implementation_1.incremental_algo()
+
+
+def main():
+    dataloader('TY4Y36NK4MF7365QHHGH')
+if __name__ == "__main__":
+    main()
